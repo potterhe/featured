350b4e6ab405e0b475c694d7ef287091deb93e91
potterhe <potter.he@gmail.com>
2026-08-28 14:15:12 +0800

    #7 refactor helloCnt metrics

--- internal/server/greeter_server.go
+++ internal/server/greeter_server.go
@@ -2,14 +2,11 @@ package server
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	pb "github.com/potterhe/featured/proto/helloworld"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/prometheus"
 	"go.opentelemetry.io/otel/metric"
-	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 )
 
 var meter = otel.Meter("greeter")
@@ -21,23 +18,7 @@ type Server struct {
 }
 
 func init() {
-	initMetrics()
 	helloCnt, _ = meter.Int64Counter("hello")
-}
-
-func initMetrics() error {
-	// 2. 创建 Prometheus Exporter —— 关键：它会自动注册到默认的 /metrics 端点
-	exporter, err := prometheus.New()
-	if err != nil {
-		return fmt.Errorf("create prometheus exporter: %w", err)
-	}
-
-	// 3. 用这个 exporter 构建 MeterProvider（全局）
-	provider := sdkmetric.NewMeterProvider(
-		sdkmetric.WithReader(exporter),
-	)
-	otel.SetMeterProvider(provider)
-	return nil
 }
 
 // SayHello implements helloworld.GreeterServer
